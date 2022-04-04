0a0fde4e6e8d78449684f0f95d51379e4bf62741
aayudeorah292 <83266511+aayudeorah292@users.noreply.github.com>
2022-04-04 15:45:46 +0530

    Add files via upload

--- laptop.js
+++ laptop.js
@@ -1,3 +1,4 @@
+status="";
 img="";
 function preload(){
     img = loadImage("laptop.jpeg");
@@ -5,6 +6,8 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status:detecting objects";
 }
 function draw(){
     image(img,0,0,640,420);
@@ -13,4 +16,17 @@ function draw(){
     noFill();
     stroke("red");
     rect(30,60,450,350);
+}
+function modelLoaded(){
+    console.log("model is loaded");
+    status=true;
+    objectdetector.detect(img,gotresult);
+}
+function gotresult(error,results){
+if (error) {
+    console.log(error);
+}
+else{
+    console.log(results);
+}
 }
